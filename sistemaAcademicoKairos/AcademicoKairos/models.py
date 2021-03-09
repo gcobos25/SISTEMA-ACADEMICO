eb6857d69067daf4cd72f55b5c8734c50b3950ba
[AK-47-SOIJ] <jioska_one@outlook.es>
2021-03-09 17:27:22 -0500

    add models confiusuario

--- sistemaAcademicoKairos/AcademicoKairos/models.py
+++ sistemaAcademicoKairos/AcademicoKairos/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class ConfiguracionUsuario(models.Model):
+    usuario = models.CharField(max_length=45,unique=True)
+    clave = models.CharField(max_length=45)
+
+    class Meta:
+        verbose_name = 'Usuario',
+        verbose_name_plural = 'Usuarios',
+        
+    def __str__(self):
+        return self.usuario
